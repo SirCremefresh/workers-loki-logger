06fa302d117d4a5c70adb87b9b954fda66d79fdd
donatowolfisberg <donato@wolfisberg.dev>
2022-04-21 22:51:31 +0200

    more tests

--- src/error-formatter.spec.ts
+++ src/error-formatter.spec.ts
@@ -1,8 +1,18 @@
 import tap from 'tap';
 import {formatErrorToString} from './error-formatter';
 
-await tap.test('top level test', async (t) => {
-  const errorString = formatErrorToString(new Error('test'));
-  t.match(errorString, /error=test, type=Error, stack=Error: test/);
+await tap.test('should format Error object', async (t) => {
+  const errorString = formatErrorToString(new Error('some-message'));
+  t.match(errorString, /error=some-message, type=Error, stack=Error: some-message/);
   t.match(errorString, /at Test\.<anonymous> \(file.*error-formatter\.spec\.ts/);
+});
+
+await tap.test('should format null', async (t) => {
+  const errorString = formatErrorToString(null);
+  t.equal(errorString, 'error=null');
+});
+
+await tap.test('should format undefined', async (t) => {
+  const errorString = formatErrorToString(undefined);
+  t.equal(errorString, 'error=undefined');
 });
--- src/error-formatter.ts
+++ src/error-formatter.ts
@@ -2,9 +2,9 @@ import {isNotNullOrUndefined, isNullOrUndefined} from './lib';
 
 export function formatErrorToString(error: any): string {
   if (isNullOrUndefined(error)) {
-    return ' error=' + error;
+    return 'error=' + error;
   }
-  let errorMessage = ' error=';
+  let errorMessage = 'error=';
   if (isNotNullOrUndefined(error.message)) {
     errorMessage += error.message;
     errorMessage += ', type=' + error.constructor.name;
