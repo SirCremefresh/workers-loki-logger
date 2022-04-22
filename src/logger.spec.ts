262374f4612f2671308ca938fc2b278d9e381b14
donatowolfisberg <donato@wolfisberg.dev>
2022-04-22 16:00:27 +0200

    Should set and override mdc

--- src/logger.spec.ts
+++ src/logger.spec.ts
@@ -38,3 +38,20 @@ await tap.test(`Should prefill mdc from config`, async (t) => {
   t.equal(logger.mdcFormatString(), 'foo=bar x=y ');
 });
 
+await tap.test(`Should set and override mdc`, async (t) => {
+  const logger = new Logger({
+    lokiSecret: '',
+    stream: {},
+    mdc: {
+      foo: 'bar',
+      x: 'y',
+    },
+  });
+  logger.mdcSet('foo', 'baz');
+  logger.mdcSet('don', 'joe');
+
+  t.equal(logger.mdcGet('foo'), 'baz');
+  t.equal(logger.mdcGet('don'), 'joe');
+  t.equal(logger.mdcFormatString(), 'foo=baz x=y don=joe ');
+});
+
